e99850c30f6d6d723dfa69cb386135a6eced4ef4
Ben Mather <bwhmather@bwhmather.com>
2014-10-10 12:19:01 +0100

    fix stupid typo in name of package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='successsion',
+    name='succession',
     url='github.com/bwhmather/succession',
     version='0.1.0',
     author='Ben Mather',
