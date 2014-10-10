b92823ea2b8b49a54743702dea127d37d2a4613c
Ben Mather <bwhmather@bwhmather.com>
2014-10-10 12:20:37 +0100

    bumped version number to 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='succession',
     url='github.com/bwhmather/succession',
-    version='0.1.0',
+    version='0.1.1',
     author='Ben Mather',
     author_email='bwhmather@bwhmather.com',
     maintainer='',
