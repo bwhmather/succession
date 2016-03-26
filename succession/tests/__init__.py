dd7359397b17501fe263a014f482ad7e5f70e7ac
Ben Mather <bwhmather@bwhmather.com>
2016-03-26 21:16:42 +0000

    Remove compress method.
    Can't see a good reason for not specifying how compressions should
    happen at construction

--- succession/tests/__init__.py
+++ succession/tests/__init__.py
@@ -111,29 +111,6 @@ class TestSuccession(unittest.TestCase):
         self.assertEqual(list(from_start), [1, 2, 3, 4, 5])
         self.assertEqual(list(from_end), [15])
 
-    def test_compress(self):
-        succession = Succession()
-
-        from_start = succession.iter()
-
-        for i in [1, 2, 3]:
-            succession.push(i)
-
-        from_middle = succession.iter()
-
-        succession.compress(lambda items: [sum(items)])
-
-        for i in [4, 5, 6]:
-            succession.push(i)
-
-        succession.close()
-
-        from_end = succession.iter()
-
-        self.assertEqual(list(from_start), [1, 2, 3, 4, 5, 6])
-        self.assertEqual(list(from_middle), [1, 2, 3, 4, 5, 6])
-        self.assertEqual(list(from_end), [6, 4, 5, 6])
-
     def test_drop_after_push(self):
         succession = Succession(compress=lambda hd: [])
 
